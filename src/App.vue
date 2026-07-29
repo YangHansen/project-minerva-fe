ae40522192772b5fc7630dcd58d46fd1354c0893
YangHansen <yanghansen02@gmail.com>
2026-07-29 22:15:24 +0700

    Add eden to connect to back-end

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+import { treaty } from '@elysiajs/eden'
+// Import the type signature directly from the server folder
+import type { App } from '../../server/src/index'
+
+
+const client = treaty<App>('http://localhost:3000')
+const status = ref('')
+
+onMounted(async () => {
+  const { data } = await client.api.health.get()
+  if (data) {
+    status.value = data.status
+  }
+})
 </script>
 
 <template>
-  <HelloWorld />
+  <main>
+    <p>API Status: <strong>{{ status }}</strong></p>
+  </main>
 </template>
